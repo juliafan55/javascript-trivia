5096cd954e95e6df9b8b3361f403e950d63c603e
juliafan55 <juliafan55@gmail.com>
2022-04-06 15:08:02 -0400

    Add DOM elements to JS, question divs and rlinked resources on pseudocode.txt and styles.css

---
+++ app.js
@@ -0,0 +1,40 @@
+// DOM Elements
+//Start Page Button
+const startGameBtn = document.querySelector(".startGameBtn");
+
+//Game Page Elements
+//Timer
+const timerElement = document.querySelector(".timer");
+
+//Total Points Card
+const totalPointsElement = document.querySelector(".totalPoints");
+
+
+//Column1
+const category1Element = document.querySelector(".category1");
+const questions11Element = document.querySelector(".questions1-1");
+const questions12Element = document.querySelector(".questions1-2");
+const questions13Element = document.querySelector(".questions1-3");
+const questions14Element = document.querySelector(".questions1-4");
+
+//Column2
+const category2Element = document.querySelector(".category2");
+const questions21Element = document.querySelector(".questions2-1");
+const questions22Element = document.querySelector(".questions2-2");
+const questions23Element = document.querySelector(".questions2-3");
+const questions24Element = document.querySelector(".questions2-4");
+
+//Column3
+const category3Element = document.querySelector(".category3");
+const questions31Element = document.querySelector(".questions3-1");
+const questions32Element = document.querySelector(".questions3-2");
+const questions33Element = document.querySelector(".questions3-3");
+const questions34Element = document.querySelector(".questions3-4");
+
+//Column4
+const category4Element = document.querySelector(".category4");
+const questions41Element = document.querySelector(".questions4-1");
+const questions42Element = document.querySelector(".questions4-2");
+const questions43Element = document.querySelector(".questions4-3");
+const questions44Element = document.querySelector(".questions4-4");
+
