fcf6ec0b9999069802d3d2ea1a6d4ffe4a8ceea7
juliafan55 <juliafan55@gmail.com>
2022-04-06 16:46:07 -0400

    Add functionality to start game button

--- app.js
+++ app.js
@@ -11,39 +11,19 @@ const timerElement = document.querySelector(".timer");
 //Total Points Card
 const totalPointsElement = document.querySelector(".totalPoints");
 
-//Column1
-const category1Element = document.querySelector(".category1");
-const questions11Element = document.querySelector(".questions1-1");
-const questions12Element = document.querySelector(".questions1-2");
-const questions13Element = document.querySelector(".questions1-3");
-const questions14Element = document.querySelector(".questions1-4");
+//Categories and Answers
 
-//Column2
-const category2Element = document.querySelector(".category2");
-const questions21Element = document.querySelector(".questions2-1");
-const questions22Element = document.querySelector(".questions2-2");
-const questions23Element = document.querySelector(".questions2-3");
-const questions24Element = document.querySelector(".questions2-4");
 
-//Column3
-const category3Element = document.querySelector(".category3");
-const questions31Element = document.querySelector(".questions3-1");
-const questions32Element = document.querySelector(".questions3-2");
-const questions33Element = document.querySelector(".questions3-3");
-const questions34Element = document.querySelector(".questions3-4");
+// add functionality to Start Game button on Start Page
 
-//Column4
-const category4Element = document.querySelector(".category4");
-const questions41Element = document.querySelector(".questions4-1");
-const questions42Element = document.querySelector(".questions4-2");
-const questions43Element = document.querySelector(".questions4-3");
-const questions44Element = document.querySelector(".questions4-4");
+startGameBtn.addEventListener("click", startGame)
 
-//add functionality to Start Game button on Start Page
-// gamePage.classList.add(".hide")
+function startGame(){
+    console.log("start game");
+    startPage.classList.add("hide");
+    gamePage.classList.remove("hide");
+}
 
-// startGameBtn.addEventListener("click", function(){
-//     console.log("start button works")
-//     startPage.classList.add(".hide");
-//     gamePage.classList.remove(".hide");
-// });
+// function selectAnswer(){
+
+// }
