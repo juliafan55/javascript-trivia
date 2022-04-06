caecfb4772349aae042937db60dbf8e37f7b33aa
juliafan55 <juliafan55@gmail.com>
2022-04-06 19:01:46 -0400

    Place question on the container, researching ways to loop over question array

--- app.js
+++ app.js
@@ -11,29 +11,11 @@ const timerElement = document.querySelector(".timer");
 //Total Points Card
 const totalPointsElement = document.querySelector(".totalPoints");
 
-//Categories and Answers
+//Questions and Answers
+let questionElement = document.querySelector(".question");
+let answerElement = document.querySelectorAll(".answerBtn");
 
-
-// add functionality to Start Game button on Start Page
-startGameBtn.addEventListener("click", startGame)
-
-//function to start game - hide start page, show game page, start timer
-function startGame(){
-    // console.log("start game");
-    startPage.classList.add("hide");
-    gamePage.classList.remove("hide");
-    nextQuestion();
-}
-
-function nextQuestion (){
-
-}
-
-
-function selectAnswer(){
-
-}
-
+//list of questions
 const listOfQuestions =[
     {
         question: "How many tails does a cat have?",
@@ -43,25 +25,56 @@ const listOfQuestions =[
             { text: 8, correct: false},
             { text: 10, correct: false},
         ]
-    }
-    {
-        question: "How many planets are there?",
-        answers: [
-            { text: 4, correct:true },
-            { text: 6, correct: false},
-            { text: 8, correct: false},
-            { text: 10, correct: false},
-        ]
-    }
-    {
-        question: "What is the total number of Zodiac Signs?",
-        answers: [
-            { text: 6, correct:true },
-            { text: 8, correct: false},
-            { text: 10, correct: false},
-            { text: 12, correct: false},
-        ]
-    }
+    },
+    // {
+    //     question: "How many planets are there?",
+    //     answers: [
+    //         { text: 4, correct:true },
+    //         { text: 6, correct: false},
+    //         { text: 8, correct: false},
+    //         { text: 10, correct: false},
+    //     ]
+    // },
+    // {
+    //     question: "What is the total number of Zodiac Signs?",
+    //     answers: [
+    //         { text: 6, correct:true },
+    //         { text: 8, correct: false},
+    //         { text: 10, correct: false},
+    //         { text: 12, correct: false},
+    //     ]
+    // }
 ]
+
+// add functionality to Start Game button on Start Page
+startGameBtn.addEventListener("click", startGame)
+
+//function to start game - hide start page, show game page - stretch: set timer and points tracker
+function startGame(){
+    // console.log("start game");
+    startPage.classList.add("hide");
+    gamePage.classList.remove("hide");
+    nextQuestion();
+}
+
+//placing the next question on the screen - stretch: shuffle the questions
+function nextQuestion (){
+    placeQuestion()
+}
+
+//places que question on the container
+function placeQuestion (question){
+    questionElement.innerText = listOfQuestions[0].question;
+    question.answers.forEach(answer => {
+
+    })
+}
+
+// addeventlistener to next button to increment listOfQuestions by 1
+
+function selectAnswer(){
+
+}
+
 
 // factory functions classes toc reate diff categories
