02bd6f5f6f2d490dc89afba16efea4e81fee2eff
juliafan55 <juliafan55@gmail.com>
2022-04-07 19:00:43 -0400

    Pseudocode further, now has basic functionality and currently still working on javascript

--- app.js
+++ app.js
@@ -1,125 +1,106 @@
-// DOM Elements
-//Start Page Elements
-const startGameBtn = document.querySelector(".startGameBtn");
-const startPage = document.querySelector(".startPage");
-const gamePage = document.querySelector(".gamePage");
+// RETHINKING THIS
+//need:
 
-//Game Page Elements
-//Timer
-const timerElement = document.querySelector(".timer");
+//event listener to startGameBtn
+//event listener to nextBtn
 
-//Total Points Card
-const totalPointsElement = document.querySelector(".totalPoints");
+// startGame()
+// starts the game
+// remove start button (when button is clicked, game starts)
+// shuffle through the questions, randomly (?)
+// set currentquestion at 0 - will need current question variable
+// make the questions appear
+// place the next question (function)
 
-//Questions and Answers
-let questionElement = document.querySelector(".question");
-let answerBtn = document.querySelectorAll(".answerBtn")
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
-//Ray's suggestion - array from nodelist
-// let answerElement = Array.from(document.querySelectorAll(".answerBtn"));
-let option1Element = document.querySelector(".option1");
-let option2Element = document.querySelector(".option2");
-let option3Element = document.querySelector(".option3");
-let option4Element = document.querySelector(".option4");
+//placeQuestion()
+//reset the game statue/clear the text/buttons
+//show the next question
 
-//global variables -- stretch score and timer
-let currentQuestion;
-// let availableQuestions = []
-// let correctAnswer = true
+//showQuestion()
+//change the innertext of the question element to the one in the array
+//loop through all the answers for each question
+//create buttons for each answer 
+//put the text of each answer into the button
 
-//list of questions
-let questionIndex = 0
-let listOfQuestions =[
+//selectAnswer()
+//grab which button was selected
+//check if the selected button is correct
+//how do i store the information if the selected button is correct??
+//if answer is correct - show green
+//if answer is incorrect - show red
+//if there are more questions: questionsList.length > currentquestion + 1
+//show the next button
+//next button needs to increment 1 to the currentquestion
+//call placeQuestion()
+//if no more question
+//start button (hard reset)
+
+//getting all required elements via DOM
+const startBtn = document.querySelector(".startBtn");
+const startPage = document.querySelector(".startPage")
+const nextBtn = document.querySelector(".nextBtn")
+const gamePage = document.querySelector(".gamePage")
+
+//global variable, check which question we are at right now
+let currentQuestionIndex = 0;
+
+//array of all the questions and answers
+let questions = [
     {
-        question: "How many tails does a cat have?",
-        options: ["A. 1 TAIL", 
-                  "B. 2 TAILS", 
-                  "C. 3 TAILS", 
-                  "D. 4 TAILS"],
-        answer: "A. 1 TAIL"
+        question: "What is 2+2?",
+        options: ["4", "5", "45", "22"],
+        answer: "4"
     },
-    // {
-    //     question: "How many planets are there?",
-    //     answers: [
-    //         { text: 4, correct:true },
-    //         { text: 6, correct: false},
-    //         { text: 8, correct: false},
-    //         { text: 10, correct: false},
-    //     ]
-    // },
-    // {
-    //     question: "What is the total number of Zodiac Signs?",
-    //     answers: [
-    //         { text: 6, correct:true },
-    //         { text: 8, correct: false},
-    //         { text: 10, correct: false},
-    //         { text: 12, correct: false},
-    //     ]
-    // }
+    {
+        question: "How many states in United States?",
+        options: ["20", "30", "40", "50"],
+        answer: "50"
+    },
+    {
+        question: "What does CSS stand for?",
+        options: ["Common Style Sheet", "Colorful Style Sheet", "Compyter Style Sheet", "Cascading Style Sheet"],
+        answer: "Cascading Style Sheet"
+    }
 ]
 
-//Event listeners
-//start button on start page
-startGameBtn.addEventListener("click", startGame);
+// if the start button is clicked -> brings to main game page -> starts the game
+startBtn.addEventListener("click", () => {
+    // console.log("start button works")
+   startPage.classList.add("hide"); //hides the start button
+   gamePage.classList.remove("hide"); //unhides the game page
+   startGame(0) //initializes the startGame function
+})
 
-
-//function to start game - hide start page, show game page - stretch: set timer and points tracker
-function startGame(){
-    // console.log("start game");
-    startPage.classList.add("hide");
-    gamePage.classList.remove("hide");
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
-    // availableQuestions = [...listOfQuestions]
-    currentQuestion = 0;
-    placeQuestion();
-}
-
-//places the question on the container
-function placeQuestion (){
-    for(let i = 0; i < listOfQuestions.length; i++){
-        questionElement.innerText = listOfQuestions[questionIndex].question;
-        option1Element.innerText = listOfQuestions[questionIndex].options[0];
-        option2Element.innerText = listOfQuestions[questionIndex].options[1];
-        option3Element.innerText = listOfQuestions[questionIndex].options[2];
-        option4Element.innerText = listOfQuestions[questionIndex].options[3];
+//if the next button is clicked -> increment question by 1 (if there are any questions left in the array), show next question
+nextBtn.addEventListener("click", () => {
+    // console.log("next button works")
+    if(currentQuestionIndex < questions.length -1 ){ //if number of the current question is less than the number of the questions array
+        currentQuestionIndex++; //increment current question by one
+        startGame(currentQuestionIndex); //initialize next game by using the current question above
+    } else {
+        // console.log("game ended")
     }
+})
 
-    //Checks Answer
-    option1Element.addEventListener("click", function(e){
-        if(e.target.innerText === listOfQuestions[0].answer){
-            option1Element.classList.add("green");
-        } else {
-            option1Element.classList.add("red");
-        }
-    })
+//start game function, pulls questions and answers from the questions array
+function startGame(index){
+    const questionElement = document.querySelector(".question"); //pulls question element from HTML
+    const answer1Element = document.querySelector(".opt1"); //pulls answer element from HTML
+    const answer2Element = document.querySelector(".opt2"); 
+    const answer3Element = document.querySelector(".opt3"); 
+    const answer4Element = document.querySelector(".opt4");
 
-    option2Element.addEventListener("click", function(e){
-        if(e.target.innerText === listOfQuestions[0].answer){
-            option2Element.classList.add("green");
-        } else {
-            option2Element.classList.add("red");
-        }
-    })
+    let questionText = `${questions[index].question}`; //pulls the questions from the array
+    let option1Text = `${questions[index].options[0]}`; //pulls the answers from the array
+    let option2Text = `${questions[index].options[1]}`; 
+    let option3Text = `${questions[index].options[2]}`; 
+    let option4Text = `${questions[index].options[3]}`; 
 
-    option3Element.addEventListener("click", function(e){
-        if(e.target.innerText === listOfQuestions[0].answer){
-            option3Element.classList.add("green");
-        } else {
-            option3Element.classList.add("red");
-        }
-    })
-
-    option4Element.addEventListener("click", function(e){
-        if(e.target.innerText === listOfQuestions[0].answer){
-            option4Element.classList.add("green");
-        } else {
-            option4Element.classList.add("red");
-        }
-    })
+    questionElement.innerHTML = questionText; //places the text of the array to the HTML
+    answer1Element.innerHTML = option1Text;
+    answer2Element.innerHTML = option2Text;
+    answer3Element.innerHTML = option3Text;
+    answer4Element.innerHTML = option4Text;
 }
 
 
-// addeventlistener to next button to increment listOfQuestions by 1
-// factory functions classes to create diff categories
-
-//dataset https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
